c3760a29e0c3581b1976def4cffe02073430031a
Keks <keks@htmlacademy.ru>
2025-01-28 12:26:15 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/photo-state.js
@@ -0,0 +1,6 @@
+let photos = [];
+const savePhotos = (newPhotos) => {
+  photos = newPhotos;
+};
+const getPhotoById = (id) => photos.find((photo) => photo.id === id);
+export { savePhotos, getPhotoById };
